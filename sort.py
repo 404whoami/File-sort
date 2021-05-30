c124ead2c9d1013475740e7e3158a6f589739f37
Prince <02prince.kr9@gmail.com>
2021-05-30 19:45:03 +0530

    work in progress

--- sort.py
+++ sort.py
@@ -1,44 +1,26 @@
-import shutil
 import os
+def makefolder(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+pick = input("Enter path from where to pick")
+drop = input("Enter path where to drop")
+select = int(input("Enter types of file 1. for image \n 2. for video \n 3.for audio \n 4. for pdf or doc \n 5. ""for everything to arrange"))
+try:
+    if select == 1:
+        makefolder('Images')
+        os.
+    elif select == 2:
+        pass
+    elif select == 3:
+        pass
+    elif select == 4:
+        pass
+    elif select == 5:
+        pass
+except ValueError:
+    print("you must enter a number not alphabets: ")
+files =os.listdir(pick)
 
-choose = int(input("Enter your sorting file choice: \n1. Images\n2. Audio/Music\n3. videos\n4. PDF files\nchoose: "))
-usource = input("Enter source address: ")
-udest = input("Enter destination address: ")
-files = os.listdir(usource)
-source = usource + "\\"
-dest = udest + "\\"
-
-while True:
-
-        #ForImages
-        if choose == 1:
-            for f in files:
-                if os.path.splitext(f)[1] in (".jpg", ".gif", ".png", ".jpeg"".webp", ".tiff", ".psd", ".raw", ".bmp", ".heif", ".indd", ".heic"):
-                    shutil.move(source+f, dest)
-                    break
-        #ForAudio
-        elif choose == 2:
-            for f in files:
-                if os.path.splitext(f)[1] in (".mp3", ".wav"):
-                    shutil.move(source+f, dest)
-                    break
-        #ForVideos
-        elif choose == 3:
-            for f in files:
-                if os.path.splitext(f)[1] in (".3gp", ".webm", ".mpg", ".ogg", ".mp4", ".m4p", ".m4v", ".avi",".wmv",".mov", ".qt",".flv", ".swf",):
-                    shutil.move(source+f, dest)
-                    break
-        #ForPDF
-        elif choose == 4:
-            for f in files:
-                if os.path.splitext(f)[1] in (".pdf"):
-                    shutil.move(source+f, dest)
-                    break
-        break
-
-
-
-
-
-
-
+makefolder('Video')
+makefolder('Audio')
+makefolder('docs')
